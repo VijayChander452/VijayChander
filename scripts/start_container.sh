5b957d21d567a91c18244893e80979aba0c1328f
VijayChander452 <vijaychander9397@gmail.com>
2024-11-18 16:59:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull vijaychandermasi/hivecode:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vijaychandermasi/hivecode:latest

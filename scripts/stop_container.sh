b4bfa48d590e06b10c15ce686156de584d2933d2
VijayChander452 <vijaychander9397@gmail.com>
2024-11-18 23:34:02 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,4 @@
 set -e
 
-# Pull the Docker image from Docker Hub
-# containerId=`docker ps | awk -F " " '{print $1}'`
 echo "stop"
 
-# Run the Docker image as a container
-# docker rm -f $containerId
